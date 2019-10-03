0a8fec44ecdd0246886134052f306c37254518be
trofimov_d_a <trofimov_d_a@magnit.ru>
2019-10-03 16:58:02 +0300

    restyle: cmake_format

--- cmake/exports/run_CXTPL_tool.cmake
+++ cmake/exports/run_CXTPL_tool.cmake
@@ -1,21 +1,24 @@
-string (REPLACE ";" " " INPUTS "${INPUTS}")
-string (REPLACE ";" " " OUTPUTS "${OUTPUTS}")
+string(REPLACE ";" " " INPUTS "${INPUTS}")
+string(REPLACE ";" " " OUTPUTS "${OUTPUTS}")
 
 message(STATUS "CXTPL_tool_PROGRAM = ${CXTPL_tool_PROGRAM}")
 message(STATUS "input_files (for ${CXTPL_tool_PROGRAM}) = ${INPUTS}")
 message(STATUS "output_files (for ${CXTPL_tool_PROGRAM}) = ${OUTPUTS}")
-message(STATUS "CXTPL_tool_LOG_CONFIG (for ${CXTPL_tool_PROGRAM}) = ${CXTPL_tool_LOG_CONFIG}")
+message(
+  STATUS "CXTPL_tool_LOG_CONFIG (for ${CXTPL_tool_PROGRAM}) = ${CXTPL_tool_LOG_CONFIG}")
 
-execute_process(COMMAND
-  ${CXTPL_tool_PROGRAM}
-    -L "${CXTPL_tool_LOG_CONFIG}"
-    --threads ${THREADS}
-    --input_files ${INPUTS}
-    --output_files ${OUTPUTS}
-  TIMEOUT 7200 # sec
-  RESULT_VARIABLE retcode
-  ERROR_VARIABLE _ERROR_VARIABLE
-)
+execute_process(COMMAND ${CXTPL_tool_PROGRAM}
+                        -L
+                        "${CXTPL_tool_LOG_CONFIG}"
+                        --threads
+                        ${THREADS}
+                        --input_files
+                        ${INPUTS}
+                        --output_files
+                        ${OUTPUTS}
+                TIMEOUT 7200 # sec
+                RESULT_VARIABLE retcode
+                ERROR_VARIABLE _ERROR_VARIABLE)
 if(NOT "${retcode}" STREQUAL "0")
-  message( FATAL_ERROR "Bad exit status ${retcode} ${_ERROR_VARIABLE}")
+  message(FATAL_ERROR "Bad exit status ${retcode} ${_ERROR_VARIABLE}")
 endif()
